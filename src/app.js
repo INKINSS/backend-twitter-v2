c5869ac6c02c4ee8afd31db03eff0e70fa9a8201
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-03-19 10:33:23 -0600

    fix(controllers): fix the error fetching bug
    Add the fetch method to get the current tweets and pass the BEARER through the process
    
    the query could be changed from static to dynamic

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import routes from './routes/app.routes.js'
+import routes from "./routes/app.routes.js";
 import dotenv from "dotenv";
 
 dotenv.config();
--- src/controllers/twetts.controller.js
+++ src/controllers/twetts.controller.js
@@ -1,29 +1,18 @@
-import needle from "needle";
-
-const BEARER_TOKEN = decodeURIComponent(process.env.BEARER_TOKEN || "");
-
 export const getCurrentTweets = async () => {
-  const url = "https://api.twitter.com/2/tweets/search/recent";
-  const params = {
-    query: "from:twitterdev -is:retweet",
-    "tweet.fields": "author_id",
-  };
-
   try {
-    const response = await needle("get", url, params, {
+    const options = {
+      method: "GET",
       headers: {
-        "User-Agent": "v2RecentSearchJS",
-        authorization: `Bearer ${BEARER_TOKEN}`,
+        Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
       },
-    });
-
-    if (response.statusCode !== 200) {
-      throw new Error(
-        `Error en la solicitud: ${response.statusCode} - ${response.statusMessage}`
-      );
-    }
-
-    return response.body;
+    };
+    const response = await fetch(
+      "https://api.twitter.com/2/tweets/search/recent?max_results=10&query=javascript",
+      options
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
   } catch (error) {
     console.error("Error al obtener tweets:", error.message);
     throw error;
